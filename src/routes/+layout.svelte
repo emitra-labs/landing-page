3839f4d3b2b9a68a3dff4a33676c2dd9f31da5d1
Ukasyah <hi.ukasyah@gmail.com>
2024-07-29 19:20:53 +0700

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { EmitraProvider } from '@emitra-labs/ui';
+	import '@emitra-labs/ui/styles.css';
+</script>
+
+<svelte:head>
+	<title>Emitra Labs</title>
+</svelte:head>
+
+<EmitraProvider>
+	<slot />
+</EmitraProvider>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<div class="h-full flex flex-col items-center justify-center">
+	<h1 class="text-3xl font-semibold">Coming Soon</h1>
+	<p class="dimmed text-lg mt-2.5">We're working hard to bring this site to life.</p>
+</div>
